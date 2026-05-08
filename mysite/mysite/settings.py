c1380d735ee0f7621909960d3ef91b0b0f0542fe
owenschu7 <owenschu7@gmail.com>
2026-05-07 20:52:00 -0700

    a

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-=5i=ehkx-6a-##eapa%2zsinl#fn8#hksm#=r%$en7k_@up%#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
